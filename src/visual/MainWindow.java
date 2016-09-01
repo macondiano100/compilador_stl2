810ea5484928d8af6623dcb5157886519bbe57d9
Antonio Vidal <antoniovidal100@hotmail.com>
2016-09-01 01:33:13 -0500

    MainWindow#FileActions
    	Renombrado a MainWindow#Controlador_pestanias_archivos
    Anadida funcionalidad para "guardar como"

--- src/visual/MainWindow.java
+++ src/visual/MainWindow.java
@@ -7,11 +7,12 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.Writer;
 import java.nio.charset.Charset;
 import java.nio.charset.MalformedInputException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Vector;
+import java.util.HashMap;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -25,6 +26,7 @@ import javax.swing.JToolBar;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.JTextComponent;
 
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rtextarea.RTextScrollPane;
@@ -65,7 +67,7 @@ public class MainWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public MainWindow() {
-		FileActions fileActions = new FileActions();
+		Controlador_pestanias_archivos fileActions = new Controlador_pestanias_archivos();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 707, 412);
 		contentPane = new JPanel();
@@ -95,7 +97,7 @@ public class MainWindow extends JFrame {
 		toolBar.add(btnGuardar);
 		
 		JButton btnGuardarComo = new JButton("Guardar como ....");
-		//btnGuardarComo.addActionListener(fileActions::accion_guardar_como);
+		btnGuardarComo.addActionListener(fileActions::accion_guardar_como);
 		btnGuardarComo.setIcon(new ImageIcon(MainWindow.class.getResource("/javax/swing/plaf/metal/icons/ocean/floppy.gif")));
 		btnGuardarComo.setToolTipText("Guardar como...");
 		toolBar.add(btnGuardarComo);
@@ -111,9 +113,10 @@ public class MainWindow extends JFrame {
 	
 	
 	//Acciones
-	private class FileActions
+	private class Controlador_pestanias_archivos
 	{
-		private Vector<Path> opened_files=new Vector<Path>();
+		private HashMap<Path,RTextScrollPane> file_to_component=new HashMap<>();
+		private HashMap<RTextScrollPane,Path> component_to_file=new HashMap<>();
 		private void add_opened_file(File f)
 		{
 						
@@ -133,10 +136,11 @@ public class MainWindow extends JFrame {
 			try(BufferedReader reader=Files.newBufferedReader(f.toPath(),Charset.forName(charsetName)))
 			{
 				Path p = f.toPath();
-				opened_files.add(p);
 				RSyntaxTextArea textArea = new RSyntaxTextArea();
 				textArea.setPopupMenu(null);
 				RTextScrollPane sp = new RTextScrollPane(textArea);
+				file_to_component.put(p, sp);
+				component_to_file.put(sp, p);
 				tabbedPane.addTab(p.getFileName().toString(),sp);
 				textArea.read(reader,f);
 			}
@@ -148,13 +152,13 @@ public class MainWindow extends JFrame {
 			}
 			
 		}
-		/*private void set_opened_file(Path p)
+		private void modifica_pestana_archivo(Path old_path,Path new_path)
 		{
-			opened_file = p;
-			tabbedPane.setTitleAt(tabbedPane.getSelectedIndex(),p.getFileName().toString());
-			
-		}*/
-		private void accion_abrir_archivo(ActionEvent e)
+			file_to_component.put(new_path,file_to_component.get(old_path));
+			file_to_component.remove(old_path);
+			tabbedPane.setTitleAt(tabbedPane.getSelectedIndex(),new_path.getFileName().toString());
+		}
+		public void accion_abrir_archivo(ActionEvent e)
 		{
 			JFileChooser file_chooser = new JFileChooser();
 			file_chooser.showOpenDialog(MainWindow.this);
@@ -164,20 +168,24 @@ public class MainWindow extends JFrame {
 				
 			}
 		}
-		/*
-		private void accion_guardar_como(ActionEvent e)
+		public void accion_guardar_como(ActionEvent e)
+		{
+			if(tabbedPane.getSelectedIndex()>=0) guardar_como();
+		}
+		private void guardar_como()
 		{
 			JFileChooser fileChooser = new JFileChooser();
 			if(fileChooser.showSaveDialog(MainWindow.this) == JFileChooser.APPROVE_OPTION)
 			{
 				Path path=fileChooser.getSelectedFile().toPath();
 				if(!Files.exists(path) || 
-						JOptionPane.showConfirmDialog(MainWindow.this, "El archivo "+path.getFileName()+"ya existe. ¿Sobreescribir?") == JOptionPane.OK_OPTION)
+						JOptionPane.showConfirmDialog(MainWindow.this, "El archivo "+path.getFileName()+" ya existe. ¿Sobreescribir?") == JOptionPane.OK_OPTION)
 				{
 					try(Writer writer = Files.newBufferedWriter(path))
 					{
-						textArea.write(writer);
-						set_opened_file(path);					
+						JScrollPane current_component = (JScrollPane)tabbedPane.getSelectedComponent();
+						((JTextComponent)current_component.getViewport().getView()).write(writer);
+						modifica_pestana_archivo(component_to_file.get(current_component),path);					
 					} catch (IOException e1) {
 						JOptionPane.showMessageDialog(MainWindow.this,"Error de guardado","Error al Guardar Archivo",JOptionPane.ERROR_MESSAGE);
 						e1.printStackTrace();
@@ -185,9 +193,11 @@ public class MainWindow extends JFrame {
 				}
 			}
 		}
-		private void accion_guardar_archivo(ActionEvent e)
+
+		/**/
+		/*private void accion_guardar_archivo(ActionEvent e)
 		{
-			if(opened_file!=null)
+			if(file_to_component.containsValue(value))
 			{
 				try(Writer writer = Files.newBufferedWriter(opened_file))
 				{
@@ -201,8 +211,7 @@ public class MainWindow extends JFrame {
 			{
 				accion_guardar_como(e);
 			}
-		}
-		*/
+		}*/
 	}
 	
 }
